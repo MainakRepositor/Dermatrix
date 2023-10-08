cdd371a35ebcf7fa4f04b1a6b63854ae4facec47
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-10-08 10:26:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ st.write("""
 file = st.file_uploader("", type=["jpg", "png"])
 def import_and_predict(image_data, model):
         size = (300,300)    
-        image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+        image = ImageOps.fit(image_data, size, Image.Resampling.LANCZOS)
         img = np.asarray(image)
         img_reshape = img[np.newaxis,...]
         prediction = model.predict(img_reshape)
